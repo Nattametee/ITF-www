800258a4b1fbd1b3b134ca2767316e66046aa867
Nattametee <73382304+Nattametee@users.noreply.github.com>
2020-10-30 17:27:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello!<br>";
-?>
+<html>
+
+<head>
+    <title>Introduce yourself</title>
+</head>
+
+<body>
+    <h1>My History</h1>
+    <p> Name: Nattmaetee Channithikul
+        Nickname : Wave
+    </p>
+    <img src="cat.jpg"><img src="cat1.jpg"><img src="cat2.jpg">
+    <br>
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h2>The unordered list</h2>
+    <ul>
+        <li>Goodle</li>
+        <li>Yahoo</li>
+        <li>Amazon</li>
+    </ul>
+
+</body>
+
+</html>
