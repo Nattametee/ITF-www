3d4b742d44cdd557d56ad455fa6f529a5c3f2928
Nattametee <73382304+Nattametee@users.noreply.github.com>
2020-11-13 17:47:39 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'labitf63070223.mysql.database.azure.com', 'nattametee@labitf63070223', 'JKHoso84', 'ITFlab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
