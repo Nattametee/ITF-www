26dd190098d69d728617e279db3f4fb0f702c075
Nattametee <73382304+Nattametee@users.noreply.github.com>
2020-11-21 01:19:05 +0700

    Update show_01.php

--- show_01.php
+++ show_01.php
@@ -6,7 +6,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>  
   <style>
     body {
-  background-image : url:("back.jpg");
+  background-color :#FAF0E6;
   }
     table {
   background-color: white;
